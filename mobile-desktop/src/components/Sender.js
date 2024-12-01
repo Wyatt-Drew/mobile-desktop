d00d6c975d2b1a18620a2d77f13efc6bfd6943cc
wyatt-drew <wyatt.j.drew@gmail.com>
2024-12-01 02:50:19 -0600

    Update Sender.js

--- mobile-desktop/src/components/Sender.js
+++ mobile-desktop/src/components/Sender.js
@@ -19,23 +19,131 @@ import { appendRow } from "./googleSheetsService";
 const targetTable = {
     subject1: [
       {
-        pdf: "PDF5",
-        targets: ["target1"],
-        landmarks: "No Icons", // Associated landmark type
+        pdf: "PDF1",
+        targets: [
+            ["target1", "target2", "target3", "target4", "target5"],
+            ["target2", "target3", "target4", "target5", "target1"],
+            ["target3", "target4", "target5", "target1", "target2"],
+            // ["target4", "target5", "target1", "target2", "target3"],
+            // ["target5", "target1", "target2", "target3", "target4"],
+        ],
+        landmarks: "Numbers", // Associated landmark type
       },
       {
         pdf: "PDF2",
-        targets: ["target6", "target7", "target8", "target9", "target10"],
-        landmarks: "Icons", // Associated landmark type
+        targets: [
+            ["target6", "target7", "target8", "target9", "target10"],
+            ["target7", "target8", "target9", "target10", "target6"],
+            ["target8", "target9", "target10", "target6", "target7"],
+            // ["target9", "target10", "target6", "target7", "target8"],
+            // ["target10", "target6", "target7", "target8", "target9"],
+        ],
+        landmarks: "No Icons", // Associated landmark type
       },
-    ],
-    subject2: [
       {
         pdf: "PDF3",
-        targets: ["target11", "target12", "target13", "target14", "target15"],
+        targets: [
+            ["target11", "target12", "target13", "target14", "target15"],
+            ["target12", "target13", "target14", "target15", "target11"],
+            ["target13", "target14", "target15", "target11", "target12"],
+            // ["target14", "target15", "target11", "target12", "target13"],
+            // ["target15", "target11", "target12", "target13", "target14"],
+        ],
         landmarks: "Letters", // Associated landmark type
       },
+      {
+        pdf: "PDF4",
+        targets: [
+            ["target15", "target16", "target17", "target18", "target19", "target20"],
+            ["target16", "target17", "target18", "target19", "target20", "target15"],
+            ["target17", "target18", "target19", "target20", "target15", "target16"],
+            ["target18", "target19", "target20", "target15", "target16", "target17"],
+            // ["target19", "target20", "target15", "target16", "target17", "target18"],
+            // ["target20", "target15", "target16", "target17", "target18", "target19"],
+        ],
+        landmarks: "Icons", // Associated landmark type
+      },
+      {
+        pdf: "PDF5",
+        targets: [
+            ["target21", "target22", "target23", "target24", "target25"],
+            ["target22", "target23", "target24", "target25", "target21"],
+            ["target23", "target24", "target25", "target21", "target22"],
+            // ["target24", "target25", "target21", "target22", "target23"],
+            // ["target25", "target21", "target22", "target23", "target24"],
+        ],
+        landmarks: "ColorIcons", // Associated landmark type
+      },
     ],
+    
+    subject2: [
+        {
+            pdf: "PDF1",
+            targets: [
+                ["target1", "target2", "target3", "target4", "target5"],
+                ["target2", "target3", "target4", "target5", "target1"],
+                ["target3", "target4", "target5", "target1", "target2"],
+                // ["target4", "target5", "target1", "target2", "target3"],
+                // ["target5", "target1", "target2", "target3", "target4"],
+            ],
+            landmarks: "No Icons", // Associated landmark type
+          },
+          {
+            pdf: "PDF2",
+            targets: [
+                ["target6", "target7", "target8", "target9", "target10"],
+                ["target7", "target8", "target9", "target10", "target6"],
+                ["target8", "target9", "target10", "target6", "target7"],
+                // ["target9", "target10", "target6", "target7", "target8"],
+                // ["target10", "target6", "target7", "target8", "target9"],
+            ],
+            landmarks: "Letters", // Associated landmark type
+          },
+          {
+            pdf: "PDF3",
+            targets: [
+                ["target11", "target12", "target13", "target14", "target15"],
+                ["target12", "target13", "target14", "target15", "target11"],
+                ["target13", "target14", "target15", "target11", "target12"],
+                // ["target14", "target15", "target11", "target12", "target13"],
+                // ["target15", "target11", "target12", "target13", "target14"],
+            ],
+            landmarks: "Icons", // Associated landmark type
+          },
+          {
+            pdf: "PDF4",
+            targets: [
+                ["target15", "target16", "target17", "target18", "target19", "target20"],
+                ["target16", "target17", "target18", "target19", "target20", "target15"],
+                ["target17", "target18", "target19", "target20", "target15", "target16"],
+                ["target18", "target19", "target20", "target15", "target16", "target17"],
+                // ["target19", "target20", "target15", "target16", "target17", "target18"],
+                // ["target20", "target15", "target16", "target17", "target18", "target19"],
+            ],
+            landmarks: "ColorIcons", // Associated landmark type
+          },
+          {
+            pdf: "PDF5",
+            targets: [
+                ["target21", "target22", "target23", "target24", "target25"],
+                ["target22", "target23", "target24", "target25", "target21"],
+                ["target23", "target24", "target25", "target21", "target22"],
+                // ["target24", "target25", "target21", "target22", "target23"],
+                // ["target25", "target21", "target22", "target23", "target24"],
+            ],
+            landmarks: "Numbers", // Associated landmark type
+          },
+        ],
+    
+        subject3: [
+            {
+                pdf: "PDF1",
+                targets: [
+                    ["target1", "target2", "target3", "target4", "target5"],
+                ],
+                landmarks: "Numbers", // Associated landmark type
+              },
+            ],
   };
 
 
@@ -260,7 +368,7 @@ const [currentScreen, setCurrentScreen] = useState(SCREENS.QR_CODE);
     } else {
         console.log("All targets in the current PDF processed. Moving to NASATLX.");
         setCurrentScreen(SCREENS.NASATLX);
-        sendMessage("TARGET", -999999);
+        sendMessage("TARGET", "NULL");
     }
 };
 
